60578879debd365df9f0f2072ec13954d012854b
pkala7968 <pratyushkala.79@gmail.com>
2025-07-01 14:26:58 +0530

    app changes

--- main.py
+++ main.py
@@ -19,10 +19,57 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 st.title("News Research Tool 📈")
 st.sidebar.title("News URLs")
 
+urls= []
 for i in range(3):
     url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
 
 process_urls= st.sidebar.button("Load URLS")
 
+main_placeholder = st.empty()
+
 if process_urls:
-    pass
+    #load data
+    loader= UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Loading data, please wait...")
+    data= loader.load()
+    #split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        separators=["\n\n", "\n", ".", ","]
+    )
+    main_placeholder.text("Splitting data into chunks...")
+    docs = text_splitter.split_documents(data)
+    #create embeddings
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    #create vectorstore
+    main_placeholder.text("Creating vectorstore...")
+    time.sleep(2) 
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorindex = FAISS.load_local(
+        "faiss_index",
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+query = main_placeholder.text_input("Enter your query:")
+if query:
+    if os.file.exists("faiss_index"):
+        with open("faiss_index", "rb") as f:
+            vectorindex = pickle.load(f)
+            chain= RetrievalQAWithSourcesChain.from_llm(
+                llm=GoogleGenerativeAI(model="gemini-2.0-flash", api_key=GOOGLE_API_KEY, temperature=0.5, max_output_tokens=250),
+                retriever=vectorindex.as_retriever()
+            )
+            result= chain({"question": query}, return_only_outputs=True)
+            main_placeholder.text("Processing your query...")
+
+            st.subheader("Results:")
+            st.write(result['answer'])
+
+            sources= result.get('sources', [])
+            if sources:
+                st.subheader("Sources:")
+                sources_list= sources.split("\n")
+                for source in sources_list:
+                    st.write(source)
